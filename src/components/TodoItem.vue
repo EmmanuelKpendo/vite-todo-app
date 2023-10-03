714f48024fbbcdf9512793cbd62b32ed04963bea
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-10-03 09:22:31 +0000

    feat: added edit functionality

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,13 +2,17 @@
 import { useTodoStore } from '../store';
 import {addIcons} from 'oh-vue-icons';
 import { MdCheckRound } from "oh-vue-icons/icons";
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 const props = defineProps(['title', 'id', 'status']);
 const store = useTodoStore();
 addIcons(MdCheckRound);
 
 const inputRef  = ref(props.title)
+
+watch(inputRef, () => {
+  store.updateTodo(inputRef.value, props.id)
+})
 
 </script>
 
@@ -21,7 +25,7 @@ const inputRef  = ref(props.title)
       <button @click="store.toggleTodoStatus(id)" class="hover:border-primary h-5 w-5 rounded-full border flex items-center justify-center">
         <v-icon name='md-check-round' v-if="status === 'completed' " scale='0.7' />
       </button>
-      <input type="text" v-model="inputRef" @input="store.updateTodo($event?.target?.value, id)" class="outline-slate-300 px-2 w-full" :disabled="status === 'completed'" :class=" status === 'completed' ? 'pointer-events-none bg-transparent' : '' " />
+      <input type="text" v-model="inputRef" class="outline-slate-300 px-2" :disabled="status === 'completed'" :class=" status === 'completed' ? 'pointer-events-none bg-transparent' : '' " />
     </div>
     <button
     @click="store.deleteTodo(id)"
